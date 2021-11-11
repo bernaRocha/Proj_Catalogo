61ac98dbaa05acc637df3004c0e936570bb22cc1
bernaRocha <mrochabernardo@gmail.com>
2021-11-10 23:00:05 -0200

    alterações do dia 10

--- index.js
+++ index.js
@@ -2,27 +2,42 @@
 
 const express = require("express");
 const app = express()
+const path = require('path')
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs"); 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }))
+
+
+// ******  Rotas
 
 app.post('/cadastro', (req, res) => {  // CREATE
    res.render('cadastro', {
 
    })
+
+   res.redirect('/')
 })
 
-app.get('/home', (req, res) => {  // READ
+app.get('/', (req, res) => {  // READ página home
+   res.render('home')
+})
 
+app.get('/cadastro', (req, res) => {  // READ páginade cadastros
+   res.render('cadastro')
 })
 
 app.get('/detalhes/:id', (req, res) => { // READ
 
 })
 
+app.patch('/detalhes/:id', (req, res) => {  // UPDATE
+
+})
 
 app.delete('/detalhes/:id', (req, res) => {  // DELETE
     
 })
 
-
-app.patch('/detalhes/:id', (req, res) => {  // UPDATE
-
-})
+app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
